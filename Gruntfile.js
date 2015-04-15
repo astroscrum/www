da027c1591716a25fce193c7e91820d5496ed4bb
Ben Arent <info@benarent.co.uk>
2015-04-14 19:16:02 -0700

    Little Grunt Server running

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,38 @@
 module.exports = function(grunt) {
 
+  // Load S3 plugin
+  grunt.loadNpmTasks('grunt-aws');
+
+  // Static Webserver
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
+    aws: grunt.file.readJSON('.aws.json'),
+    s3: {
       options: {
-        separator: ';'
+        accessKeyId: "<%= aws.accessKeyId %>",
+        secretAccessKey: "<%= aws.secretAccessKey %>",
+        bucket: "<%= aws.bucket %>"
       },
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+      build: {
+        cwd: "dist",
+        src: "**"
       }
     },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: "src",
+          keepalive: true
+        }
+      }
+    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-
+  // Default task(s).
+  grunt.registerTask("default", ["connect"]);
 
 };
